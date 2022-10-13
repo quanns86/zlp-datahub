66829fce7897581453b7dde4ee64f208f497c207
Hung <hungnp5@vng.com.vn>
2022-10-14 00:52:14 +0700

    feat: sync datahub-master

--- datahub-actions/run_ingest.sh
+++ datahub-actions/run_ingest.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-# usage: ./run_ingest.sh <task-id> <datahub-version> <plugins-required> <tmp-dir> <recipe_file> <report_file>
+# usage: ./run_ingest.sh <recipe-id> <datahub-version> <plugins-required> <tmp-dir>
 
-# set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR" || exit
+cd "$DIR"
 
-task_id="$1"
-datahub_version="$2"
-plugins="$3"
-tmp_dir="$4"
-recipe_file="$5"
-report_file="$6"
-debug_mode="$7"
-
-# Execute DataHub recipe, based on the recipe id.
-export JAVA_HOME=/home/datahub/.sdkman/candidates/java/current
-echo "datahub ingest run -c ${recipe_file}"
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
-if (datahub ingest run -c "${recipe_file}"); then
+# Execute DataHub recipe, based on the recipe id. 
+if (python3 -m datahub ingest -c "$4/$1.yml"); then
   exit 0
 else
   exit 1
