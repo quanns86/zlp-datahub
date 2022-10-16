031e32f0459ae56dd984992248cefdd57f2877ab
hungnp5 <hungnp5@vng.com.vn>
2022-10-16 14:07:55 +0700

    actions framework

--- datahub-actions/run_ingest.sh
+++ datahub-actions/run_ingest.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-# usage: ./run_ingest.sh <recipe-id> <datahub-version> <plugins-required> <tmp-dir>
+# usage: ./run_ingest.sh <task-id> <datahub-version> <plugins-required> <tmp-dir> <recipe_file> <report_file>
 
+set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
+cd "$DIR" || exit
 
-# Execute DataHub recipe, based on the recipe id. 
-if (python3 -m datahub ingest -c "$4/$1.yml"); then
+task_id="$1"
+datahub_version="$2"
+plugins="$3"
+tmp_dir="$4"
+recipe_file="$5"
+report_file="$6"
+debug_mode="$7"
+
+# Execute DataHub recipe, based on the recipe id.
+echo "datahub  ingest run -c ${recipe_file}"
+if (datahub ingest run -c "${recipe_file}"); then
   exit 0
 else
   exit 1
